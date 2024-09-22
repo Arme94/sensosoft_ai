c071b07640979265d1773276276472cae31c9301
Arme94 <arnoldmenss@gmail.com>
2024-09-21 21:44:38 -0500

    se agregan vistas base

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('home/', views.home, name='home'),
+    path('users/', views.users, name='users'),
+    path('reports/', views.reports, name='reports'),
+    path('panel/', views.panel, name='panel'),
+    path('about/', views.about, name='about'),
 ]
