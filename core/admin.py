5b507577bf2f350655b0c333ceb74c4fe3159f4f
Arme94 <arnol.meneses00@usc.edu.co>
2024-09-21 19:44:14 -0500

    Revert "Login frontend"

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cerveza
-
-@admin.register(Cerveza)
-class CervezaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'aroma', 'sabor', 'color', 'textura', 'fecha_creacion')
-
--- core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cerveza
-
-class CervezaForm(forms.ModelForm):
-    class Meta:
-        model = Cerveza
-        fields = ['nombre', 'aroma', 'sabor', 'color', 'textura']
