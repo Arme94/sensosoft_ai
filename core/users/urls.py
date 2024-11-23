ef8bba937cbcf6942b50dfd49c2fc272a9643d16
Arme94 <arnoldmenss@gmail.com>
2024-11-22 20:47:27 -0500

    ajsutes en el panel para pintar graficas

--- core/users/urls.py
+++ core/users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('detail/<int:id>', login_required(views.user_detail), name='user_detail'),
     path('user_update/<int:id>', login_required(views.user_update), name='user_update'),
     path('delete/<int:id>', login_required(views.user_delete), name='user_delete'),
+    path('update_user_info/<int:id>', login_required(views.update_user_info), name='update_user_info'),
 ]
